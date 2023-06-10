a63521eb49b550696c1fbc80d78c9e08d2b03daa
calltrum <chideraomeje190@gmail.com>
2023-06-10 16:38:54 +0100

    nato phonetic alphabet codes

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -25,9 +25,6 @@
 # Keyword Method with iterrows()
 
 
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 import pandas
 
 nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
